57747e1dd735cecfc8c3229cdf4c1b034073a3d4
AzeGab <gabrielgil7g@gmail.com>
2025-05-23 11:12:29 +0100

    feat : modif json et requete dans transfert_chantiers_vers_batisimply

--- app/services/chantier.py
+++ app/services/chantier.py
@@ -4,7 +4,11 @@
 # des chantiers depuis SQL Server vers PostgreSQL
 
 import psycopg2
-from app.services.connex import connect_to_sqlserver, connect_to_postgres, load_credentials
+from app.services.connex import connect_to_sqlserver, connect_to_postgres, load_credentials, recup_batisimply_token
+
+import requests
+import json
+from datetime import date
 
 def transfer_chantiers():
     """
@@ -48,23 +52,26 @@ def transfer_chantiers():
 
         # Récupération des chantiers depuis SQL Server
         sqlserver_cursor.execute("""
-            SELECT Code, DateDebut, DateFin, NomClient, Libelle, AdrChantier, CPChantier, VilleChantier 
+            SELECT ChantierDef.Code, DateDebut, DateFin, NomClient, Libelle, AdrChantier, CPChantier, VilleChantier, SUM(TempsMO) AS "TotalMo"
             FROM dbo.ChantierDef
+            JOIN Devis ON devis.CodeClient = ChantierDef.CodeClient
+            WHERE Devis.Etat = 3
+            GROUP BY chantierDef.Code, DateDebut, DateFin, NomClient, Libelle, AdrChantier, CPChantier, VilleChantier
         """)
         rows = sqlserver_cursor.fetchall()
 
         # Insertion des chantiers dans PostgreSQL
         for row in rows:
-            code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier = row
+            code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier, TotalMo = row
             # Utilisation de ON CONFLICT pour éviter les doublons
             postgres_cursor.execute(
                 """
                 INSERT INTO batigest_chantiers 
-                (code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier)
-                VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+                (code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier, total_mo)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
                 ON CONFLICT (code) DO NOTHING
                 """,
-                (code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier)
+                (code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier, TotalMo)
             )
 
         # Validation des modifications dans PostgreSQL
@@ -81,3 +88,91 @@ def transfer_chantiers():
     except Exception as e:
         # En cas d'erreur, on retourne le message d'erreur
         return False, f"❌ Erreur lors du transfert : {e}"
+
+
+def transfer_chantiers_vers_batisimply():
+    token = recup_batisimply_token()
+    if not token:
+        print("Impossible de continuer sans token.")
+        return False
+
+    creds = load_credentials()
+    if not creds or "postgres" not in creds:
+        print("❌ Informations Postgres manquantes.")
+        return False
+
+    pg = creds["postgres"]
+    postgres_conn = connect_to_postgres(
+        pg["host"], pg["user"], pg["password"], pg["database"], pg.get("port", "5432")
+    )
+
+    if not postgres_conn:
+        print("❌ Connexion à la base Postgres échouée.")
+        return False
+
+    API_URL = "https://api.staging.batisimply.fr/api/project"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+
+    postgres_cursor = postgres_conn.cursor()
+
+    postgres_cursor.execute("""
+        SELECT code, date_debut, date_fin, nom_client, description, adr_chantier, cp_chantier, ville_chantier, total_mo
+        FROM batigest_chantiers
+        WHERE sync = false
+    """)
+
+    rows = postgres_cursor.fetchall()
+    success = False  # <- Flag de succès
+
+    for row in rows:
+        code, date_debut, date_fin, nom_client, description, adr, cp, ville, total_mo = row
+
+        data = {
+            "address": {
+                "city": ville,
+                "countryCode": "FR",
+                "geoPoint": {
+                    "xLon": 3.8777,
+                    "yLat": 43.6119
+                },
+                "googleFormattedAddress": f"{adr}, {cp} {ville}, France",
+                "postalCode": cp,
+                "street": adr
+            },
+            "budget": {
+                "amount": 500000.0,
+                "currency": "EUR"
+            },
+            "endEstimated": date_fin.strftime("%Y-%m-%d") if date_fin else None,
+            "headQuarter": {
+                "id": 33
+            },
+            "hoursSold": total_mo,
+            "projectCode": code,
+            "comment": description,
+            "projectName": nom_client,
+            "customerName":nom_client,
+            "projectManager": "DEFINIR",
+            "startEstimated": date_debut.strftime("%Y-%m-%d") if date_debut else None,
+            "isArchived": False,
+            "isFinished": False,
+            "projectColor": "#9b1ff1"
+        }
+
+        response = requests.post(API_URL, headers=headers, data=json.dumps(data))
+        if response.status_code == 201:
+            print(f"✅ Projet '{code}' envoyé avec succès")
+            postgres_cursor.execute(
+                "UPDATE batigest_chantiers SET sync = true, sync_date = NOW() WHERE code = %s", (code,))
+            postgres_conn.commit()
+            success = True
+        else:
+            print(f"❌ Erreur pour le projet '{code}' : {response.status_code} → {response.text}")
+
+    postgres_cursor.close()
+    postgres_conn.close()
+
+    return success 
